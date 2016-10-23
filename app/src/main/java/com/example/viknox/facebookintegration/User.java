3193f6e35eaded339fc9e69dbd0dd8359b21ac5b
DESKTOP-QGR187P\x230 <viknox1912@gmail.com>
2016-10-23 20:26:47 +0200

    level 1
    First Stage: Integrating Facebook SDK

---
+++ app/src/main/java/com/example/viknox/facebookintegration/User.java
@@ -0,0 +1,12 @@
+package com.example.viknox.facebookintegration;
+
+/**
+ * Created by x230 on 10/23/2016.
+ */
+public class User {
+    String Name;
+    String Age;
+    String Email;
+    String gender;
+
+}
