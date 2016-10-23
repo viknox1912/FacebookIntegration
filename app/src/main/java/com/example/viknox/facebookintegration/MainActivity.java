2978a4c006f1e732d1ce9d898426a1e0e46c0db7
DESKTOP-QGR187P\x230 <viknox1912@gmail.com>
2016-10-23 20:39:47 +0200

    User Class
    Userclastoo store user info and initializing Facebook SDK

--- app/src/main/java/com/example/viknox/facebookintegration/MainActivity.java
+++ app/src/main/java/com/example/viknox/facebookintegration/MainActivity.java
@@ -2,13 +2,47 @@ package com.example.viknox.facebookintegration;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
+    LoginButton bt_login;
+    TextView tv_login;
+    private FacebookCallback<LoginResult> fbCallback = new FacebookCallback<LoginResult>() {
+        @Override
+        public void onSuccess(LoginResult loginResult) {
+            //TODO 1. Get UserName, AuthToken,DoB,Email,PicURL
+        }
+
+        @Override
+        public void onCancel() {
+            Toast
+        }
+
+        @Override
+        public void onError(FacebookException error) {
+
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        FacebookSdk.sdkInitialize(getApplicationContext());
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+        initUI();
+
     }
+
+    private void initUI() {
+        bt_login = (LoginButton) findViewById(R.id.login_button);
+        tv_login = (TextView)    findViewById(R.id.tv_hello);
+    }
+
 }
--- app/src/main/java/com/example/viknox/facebookintegration/User.java
+++ app/src/main/java/com/example/viknox/facebookintegration/User.java
@@ -1,12 +1,64 @@
 package com.example.viknox.facebookintegration;
 
+import java.net.URL;
+
 /**
  * Created by x230 on 10/23/2016.
  */
 public class User {
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getAge() {
+        return Age;
+    }
+
+    public void setAge(String age) {
+        Age = age;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+
+    public URL getPic() {
+        return Pic;
+    }
+
+    public void setPic(URL pic) {
+        Pic = pic;
+    }
+
     String Name;
     String Age;
     String Email;
     String gender;
+    String tags;
+    URL Pic;
 
 }
