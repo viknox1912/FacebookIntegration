4a073de12ddd8e6adb2dd21996a8ccb4cffd4ecb
DESKTOP-QGR187P\x230 <viknox1912@gmail.com>
2016-10-24 11:57:38 +0200

    Overview
    Initialize UI

--- app/src/main/java/com/example/viknox/facebookintegration/OverviewActivity.java
+++ app/src/main/java/com/example/viknox/facebookintegration/OverviewActivity.java
@@ -1,13 +1,29 @@
 package com.example.viknox.facebookintegration;
 
 import android.app.Activity;
+import android.media.Image;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.widget.ImageButton;
 
 
 public class OverviewActivity extends Activity {
+    ImageButton btn_search, btn_groups, btn_chat, btn_settings;
+
+
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+        super.onCreate(savedInstanceState);
+         setContentView(R.layout.overview_activity);
+        initUI();
+    }
+
+    private void initUI() {
+        btn_search = (ImageButton) findViewById(R.id.searchBtn);
+        btn_groups = (ImageButton) findViewById(R.id.groupsBtn);
+        btn_chat   = (ImageButton) findViewById(R.id.chatBtn);
+        btn_settings = (ImageButton) findViewById(R.id.settingsBtn);
+
+
     }
 }
